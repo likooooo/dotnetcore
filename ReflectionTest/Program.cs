e196eeef93940f3facb38e2cc39a9bd7e9620345
like <462866046@qq.com>
2021-02-03 16:54:50 +0800

    正在学习wpf

---
+++ ReflectionTest/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ReflectionTest
+{
+    public class CoderDefAttribute : System.Xml.Serialization.XmlIgnoreAttribute 
+    {
+        public bool a;
+        public string b;
+        public CoderDefAttribute(bool a,string b) 
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+    }
+    class TestClass
+    {
+        [CoderDefAttribute(true,"asd")]
+        public int member;
+        public int Property{get=>member;}
+        public TestClass(){}
+        public void PrintWhoCallThisMethord()
+        {
+            MethodBase method = new System.Diagnostics.StackTrace().GetFrame(1).GetMethod();
+            Console.WriteLine(method.ReflectedType.FullName);
+        }
+    }
+    class Program
+    {
+        //https://docs.microsoft.com/zh-cn/dotnet/api/system.reflection.assembly?view=net-5.0
+        static void Main(string[] args)
+        {
+            TestClass tc = new TestClass();
+            tc.PrintWhoCallThisMethord();
+            Type t = typeof(TestClass);
+            //获取所有方法 
+            System.Reflection.MethodInfo[] methods = t.GetMethods(); 
+            Console.WriteLine("方法:");
+            foreach (var i in methods)
+            {
+                Console.WriteLine("  " + i);
+            }
+            //获取所有成员 
+            System.Reflection.MemberInfo[] members = t.GetMembers(); 
+            Console.WriteLine("成员对象:");
+            foreach (var i in members)
+            {
+                Console.WriteLine("  " + i);
+            }
+            //获取所有属性 
+            System.Reflection.PropertyInfo[] properties = t.GetProperties();
+            Console.WriteLine("索引器:");
+            foreach (var i in properties)
+            {
+                Console.WriteLine("  " + i);
+            }
+            //获取Attribute
+            var attributes = t.GetCustomAttributes(typeof(CoderDefAttribute), false);
+            Console.WriteLine("Attribute:");
+            foreach(var mInfo in members) 
+            {
+                // Iterate through all the Attributes for each method.
+                foreach (Attribute attr in Attribute.GetCustomAttributes(mInfo)) 
+                {
+                    // Check for the AnimalType attribute.
+                    if (attr.GetType() == typeof(CoderDefAttribute))
+                    {
+                        Console.WriteLine("m_Info {0} has [Attribute] : bool {1} ,string {2} .",mInfo.Name, ((CoderDefAttribute)attr).a,((CoderDefAttribute)attr).b);
+                    }
+            }
+        }
+
+            // Console.WriteLine("Assembly Full Name:");
+            // Console.WriteLine(t.FullName);
+            // Console.WriteLine("\nAssembly CodeBase:");
+            // Console.WriteLine(t.CodeBase);
+        }
+    }
+}
---
+++ Wpf/控件/Sample/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+//https://docs.microsoft.com/zh-cn/dotnet/api/system.windows.datatemplate?redirectedfrom=MSDN&view=net-5.0
+namespace Sample
+{
+    public struct Photo
+    {
+        public string Source{get;set;}
+    }
+    public class MyPhotos : ObservableCollection<Photo>
+    {
+        public MyPhotos()
+        {
+            string dir = @"C:\Users\like\Desktop\Project\dotnet-core\dotnetcore\ImageProcess\Sample\";
+            Add(new Photo{Source = dir + "570_544_24.bmp"});
+            Add(new Photo{Source = dir + "637_475_1.bmp"});
+            Add(new Photo{Source = dir + "637_475_8.bmp"});
+            Add(new Photo{Source = dir + "637_475_16.bmp"});
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            listBox1.ItemsSource = (new MyPhotos()).ToList<Photo>();
+            // var v = myPhotos;
+        }
+    }
+}
